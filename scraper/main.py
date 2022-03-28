1c6f3908f32c82983937f39977db151d6cc761c0
LICHKING <savitarreus@gmail.com>
2022-03-28 11:31:50 +0300

    ❄️ Trying to run google chrome on Selenium ❄️

--- scraper/main.py
+++ scraper/main.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
+# Instagram Login:
+
+driver.get('https://www.instagram.com/')
+
+time.sleep(5)
+driver.close()
+driver.quit()
