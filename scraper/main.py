1cfd905ac9e882a4d53cb6b189d38732f6d42b9e
LICHKING <savitarreus@gmail.com>
2022-03-29 10:21:25 +0300

    ❄️ Made a functions ❄️

--- scraper/main.py
+++ scraper/main.py
@@ -10,23 +10,31 @@ from webdriver_manager.chrome import ChromeDriverManager
 from config import settings
 
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def set_up_driver() -> webdriver:
+	driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+	driver.get('https://www.instagram.com/')
+	return driver
 
 # Instagram Login:
 
-driver.get('https://www.instagram.com/')
+def instagram_log_in(driver: webdriver):
+	username_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="username"]')))
+	password_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="password"]')))
 
-username_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="username"]')))
-password_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="password"]')))
+	username_input_field.clear()
+	password_input_field.clear()
 
-username_input_field.clear()
-password_input_field.clear()
+	username_input_field.send_keys(settings.instagram_username)
+	password_input_field.send_keys(settings.instagram_password)
+	
+	log_in_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[type="submit"]'))).click()
 
-username_input_field.send_keys(settings.instagram_username)
-password_input_field.send_keys(settings.instagram_password)
+#
 
-log_in_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[type="submit"]'))).click()
 
-time.sleep(5)
-driver.close()
-driver.quit()
+if __name__ == '__main__':
+	driver = set_up_driver()
+	instagram_log_in(driver)
+	time.sleep(5)
+	driver.close()
+	driver.quit()
