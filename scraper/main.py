bb9874eee05f4c0faedaa2984a2dd55845c2f1a5
LICHKING <savitarreus@gmail.com>
2022-04-03 13:17:45 +0300

    ❄️ Black linter ❄️

--- scraper/main.py
+++ scraper/main.py
@@ -12,54 +12,72 @@ from webdriver_manager.chrome import ChromeDriverManager
 from config import settings
 
 
-class Instraper():
-	def set_up_driver(self) -> webdriver:
-		driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-		driver.get('https://www.instagram.com/')
-		return driver
+class Instraper:
+    def set_up_driver(self) -> webdriver:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get("https://www.instagram.com/")
+        return driver
 
-	def not_now_dismiss(self, driver: webdriver) -> WebDriverWait:
-		return WebDriverWait(driver, 10).until(
-			EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Не зараз")]'))
-		).click()
+    def not_now_dismiss(self, driver: webdriver) -> WebDriverWait:
+        return (
+            WebDriverWait(driver, 10)
+            .until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, '//button[contains(text(), "Не зараз")]')
+                )
+            )
+            .click()
+        )
 
-	def scraping_data_by_hashtag(self, driver: webdriver, keyword: str) -> NoReturn:
-		# Instagram Login:
+    def scraping_data_by_hashtag(self, driver: webdriver, keyword: str) -> NoReturn:
+        # Instagram Login:
 
-		username_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="username"]')))
-		password_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="password"]')))
-		username_input_field.clear()
-		password_input_field.clear()
-		username_input_field.send_keys(settings.instagram_username)
-		password_input_field.send_keys(settings.instagram_password)
-		
-		log_in_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[type="submit"]')))
-		log_in_button.click()
+        username_input_field = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="username"]'))
+        )
+        password_input_field = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="password"]'))
+        )
+        username_input_field.clear()
+        password_input_field.clear()
+        username_input_field.send_keys(settings.instagram_username)
+        password_input_field.send_keys(settings.instagram_password)
 
-		# Dismissing pop up messages:
+        log_in_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[type="submit"]'))
+        )
+        log_in_button.click()
 
-		self.not_now_dismiss(driver)
-		self.not_now_dismiss(driver)
+        # Dismissing pop up messages:
 
-		# Searching for a keyword:
+        self.not_now_dismiss(driver)
+        self.not_now_dismiss(driver)
 
-		search_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="Пошук"]')))
-		search_input_field.clear()
-		search_input_field.send_keys(keyword)
-		time.sleep(5)
-		keyword_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@href, '/" + keyword[1:] + "/')]")))
-		keyword_link.click()
+        # Searching for a keyword:
 
-		# Scrolling down:
+        search_input_field = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="Пошук"]'))
+        )
+        search_input_field.clear()
+        search_input_field.send_keys(keyword)
+        time.sleep(5)
+        keyword_link = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//a[contains(@href, '/" + keyword[1:] + "/')]")
+            )
+        )
+        keyword_link.click()
 
-		# number_of_scrolls = 2
-		# for i in range(0, number_of_scrolls):
-		# 	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		# 	time.sleep(5)
+        # Scrolling down:
+
+        # number_of_scrolls = 2
+        # for i in range(0, number_of_scrolls):
+        # 	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # 	time.sleep(5)
 
 
-if __name__ == '__main__':
-	keyword = '#z'
-	new_scrapper = Instraper()
-	driver = new_scrapper.set_up_driver()
-	new_scrapper.scraping_data_by_hashtag(driver, keyword)
+if __name__ == "__main__":
+    keyword = "#z"
+    new_scrapper = Instraper()
+    driver = new_scrapper.set_up_driver()
+    new_scrapper.scraping_data_by_hashtag(driver, keyword)
