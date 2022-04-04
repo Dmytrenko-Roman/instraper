c716233239964e74b54e85343adabf21f89f82a1
LICHKING <savitarreus@gmail.com>
2022-04-04 13:04:34 +0300

    ❄️ Black linter ❄️

--- scraper/database.py
+++ scraper/database.py
@@ -8,25 +8,33 @@ from config import settings
 
 
 class MongoDB:
-	def __init__(self, connection_string: str, db_name: str, collection_name: str):
-		self.connection_string = connection_string
-		self.cluster = MongoClient(self.connection_string)
-		self.db = self.cluster[db_name]
-		self.collection = self.db[collection_name]
+    def __init__(self, connection_string: str, db_name: str, collection_name: str):
+        self.connection_string = connection_string
+        self.cluster = MongoClient(self.connection_string)
+        self.db = self.cluster[db_name]
+        self.collection = self.db[collection_name]
 
 
 class PostCollection(MongoDB):
-	def create_post(self, post_owner: str, post_owner_url: str, post_description: str, post_image_url: str):
-		post = {
-			'owner': post_owner,
-			'owner_url': post_owner_url,
-			'description': post_description,
-			'image_url': post_image_url,
-		}
+    def create_post(
+        self,
+        post_owner: str,
+        post_owner_url: str,
+        post_description: str,
+        post_image_url: str,
+    ):
+        post = {
+            "owner": post_owner,
+            "owner_url": post_owner_url,
+            "description": post_description,
+            "image_url": post_image_url,
+        }
 
-		self.collection.insert_one(post)
+        self.collection.insert_one(post)
 
 
-CONNECTION_STRING = f'mongodb+srv://{settings.database_username}:{settings.database_password}@cluster0.slvzn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+CONNECTION_STRING = f"mongodb+srv://{settings.database_username}:{settings.database_password}@cluster0.slvzn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-post_collection = PostCollection(connection_string=CONNECTION_STRING, db_name='instraper', collection_name='post')
+post_collection = PostCollection(
+    connection_string=CONNECTION_STRING, db_name="instraper", collection_name="post"
+)
--- scraper/instraper.py
+++ scraper/instraper.py
@@ -12,74 +12,113 @@ from config import settings
 from database import post_collection
 
 
-class Instraper():
-	def set_up_driver(self) -> webdriver:
-		driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-		driver.get(url='https://www.instagram.com/')
-		return driver
+class Instraper:
+    def set_up_driver(self) -> webdriver:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get(url="https://www.instagram.com/")
+        return driver
 
-	def not_now_dismiss(self, driver: webdriver) -> WebDriverWait:
-		return WebDriverWait(driver, 10).until(
-			EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Не зараз")]'))
-		).click()
+    def not_now_dismiss(self, driver: webdriver) -> WebDriverWait:
+        return (
+            WebDriverWait(driver, 10)
+            .until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, '//button[contains(text(), "Не зараз")]')
+                )
+            )
+            .click()
+        )
 
-	def scraping_data_by_hashtag(self, driver: webdriver, keyword: str, limit: int) -> NoReturn:
-		# Instagram Login:
+    def scraping_data_by_hashtag(
+        self, driver: webdriver, keyword: str, limit: int
+    ) -> NoReturn:
+        # Instagram Login:
 
-		username_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="username"]')))
-		password_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="password"]')))
-		username_input_field.clear()
-		password_input_field.clear()
-		username_input_field.send_keys(settings.instagram_username)
-		password_input_field.send_keys(settings.instagram_password)
-		
-		log_in_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[type="submit"]')))
-		log_in_button.click()
+        username_input_field = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="username"]'))
+        )
+        password_input_field = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="password"]'))
+        )
+        username_input_field.clear()
+        password_input_field.clear()
+        username_input_field.send_keys(settings.instagram_username)
+        password_input_field.send_keys(settings.instagram_password)
 
-		# Dismissing pop up messages:
+        log_in_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[type="submit"]'))
+        )
+        log_in_button.click()
 
-		self.not_now_dismiss(driver)
-		self.not_now_dismiss(driver)
+        # Dismissing pop up messages:
 
-		# Searching for a keyword:
+        self.not_now_dismiss(driver)
+        self.not_now_dismiss(driver)
 
-		search_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="Пошук"]')))
-		search_input_field.clear()
-		search_input_field.send_keys(keyword)
-		time.sleep(5)
-		keyword_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@href, '/" + keyword[1:] + "/')]")))
-		keyword_link.click()
+        # Searching for a keyword:
 
-		# Trying to get data of each post:
+        search_input_field = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="Пошук"]'))
+        )
+        search_input_field.clear()
+        search_input_field.send_keys(keyword)
+        time.sleep(5)
+        keyword_link = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//a[contains(@href, '/" + keyword[1:] + "/')]")
+            )
+        )
+        keyword_link.click()
 
-		time.sleep(5)
-		all_posts = driver.find_elements(By.TAG_NAME, 'a')
-		all_posts_links = []
-		for post in all_posts:
-			post_link = post.get_attribute('href')
-			if '/p/' in post_link:
-				all_posts_links.append(post_link.split('.com', 1)[1])
+        # Trying to get data of each post:
 
-		post_counter = 0
-		for link in all_posts_links:
-			time.sleep(5)
-			post_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f"//a[contains(@href, '{link}')]")))
-			post_link.click()
+        time.sleep(5)
+        all_posts = driver.find_elements(By.TAG_NAME, "a")
+        all_posts_links = []
+        post_counter = 0
+        for post in all_posts:
+            post_link = post.get_attribute("href")
+            if "/p/" in post_link:
+                all_posts_links.append(post_link.split(".com", 1)[1])
 
-			time.sleep(5)
-			post_owner = driver.find_element(By.XPATH, "//a[contains(@class, 'sqdOP yWX7d     _8A5w5   ZIAjV ')]").text
-			post_owner_url = driver.find_element(By.XPATH, "//a[contains(@class, 'sqdOP yWX7d     _8A5w5   ZIAjV ')]").get_attribute('href')
-			post_description = driver.find_element(By.XPATH, "//span[contains(@class, '_7UhW9   xLCgt      MMzan   KV-D4           se6yk       T0kll ')]").text
-			post_image_url = driver.find_element(By.XPATH, "//img[contains(@class, 'FFVAD')]").get_attribute('src')
+            # Limiting the posts:
 
-			post_collection.create_post(post_owner, post_owner_url, post_description, post_image_url)
+            post_counter += 1
+            if post_counter == limit:
+                break
 
-			time.sleep(5)
-			close_the_post = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'wpO6b  ')]")))
-			close_the_post.click()
+        for link in all_posts_links:
+            time.sleep(5)
+            post_link = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, f"//a[contains(@href, '{link}')]")
+                )
+            )
+            post_link.click()
 
-			# Limiting the posts:
+            time.sleep(5)
+            post_owner = driver.find_element(
+                By.XPATH, "//a[contains(@class, 'sqdOP yWX7d     _8A5w5   ZIAjV ')]"
+            ).text
+            post_owner_url = driver.find_element(
+                By.XPATH, "//a[contains(@class, 'sqdOP yWX7d     _8A5w5   ZIAjV ')]"
+            ).get_attribute("href")
+            post_description = driver.find_element(
+                By.XPATH,
+                "//span[contains(@class, '_7UhW9   xLCgt      MMzan   KV-D4           se6yk       T0kll ')]",
+            ).text
+            post_image_url = driver.find_element(
+                By.XPATH, "//img[contains(@class, 'FFVAD')]"
+            ).get_attribute("src")
 
-			post_counter += 1
-			if post_counter == limit:
-				break
+            post_collection.create_post(
+                post_owner, post_owner_url, post_description, post_image_url
+            )
+
+            time.sleep(5)
+            close_the_post = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//button[contains(@class, 'wpO6b  ')]")
+                )
+            )
+            close_the_post.click()
